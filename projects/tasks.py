4e9987b8065fc488f4fbbbe5ed250ae48fe679a5
Raony Guimarães <raonyguimaraes@gmail.com>
2016-08-12 17:14:57 +0200

    download fastq

--- projects/tasks.py
+++ projects/tasks.py
@@ -40,7 +40,7 @@ def import_files_from_basespace(task_id):
     files = basespace.getfilelinks()
 
     print('path', os.getcwd())
-    download_path = '../work/projects/%s/tasks/%s/' (project_task.project.id, project_task.id)
+    download_path = '../work/projects/%s/tasks/%s/' % (project_task.project.id, project_task.id)
     
     if not os.path.isdir(download_path):
         os.makedirs(download_path)
