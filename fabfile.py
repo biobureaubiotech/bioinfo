1e0c60009c5a03777ffa8b2dddb990a5c184e598
Raony Guimarães <raonyguimaraes@gmail.com>
2016-08-13 13:05:37 +0200

    deploy to worker

--- fabfile.py
+++ fabfile.py
@@ -23,7 +23,8 @@ def deploy_web():
 
     run('sudo systemctl restart gunicorn')
 def deploy_worker():
-    local('git add .;git commit -am "deploy to worker;git push"')
+    local('git add .;git commit -am "deploy to worker"')
+    local('git push')
     with cd('bioinfo_biobureau'):
         run('git pull')
 
